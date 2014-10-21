f147d89d53fed20d4c9a26525031e92c462bf45c
EWB User <ewb@unassigned-hostname>
2014-10-21 17:45:42 -0400

    message

--- project.js
+++ project.js
@@ -1,27 +1,15 @@
-title('My Project');
 
-// put variables here, just like kahn academy
-var exampleVar = 100;
+title('Project-Breakout');
 
-// put functions here, just like kahn academy
-var exampleFunction = function() {
-    
-    // change color depending on key pressed
-    if (keyIsPressed && keyCode === SPACE) {
-        fill(255, 0, 0);
-    } else {
-        fill(0, 0, 255);
-    }
-
-    rect(300, 200, 55, 55);
-};
+// Variables
+var xCoord = 0;
+var yCoord = 0;
 
 // this code is executed once when the program is started
 var setup = function() {
 
     // set up the size of the canvas (you probably don't want to change this!)
-    size(800, 600);
-
+    size(400,400);
 };
 
 // override draw function, by default it will be called 60 times per second
@@ -33,14 +21,6 @@ var draw = function() {
     // some drawing commands; feel free to change these
     fill(255, 0, 0);
 
-    // draw some text
-    textSize(50);
-    text("Hello, World!", exampleVar, exampleVar);
-
-    textSize(20);
-    text("Press the space bar...", 500, 500);
-
-    // call a function (defined up above)
-    exampleFunction();
-
+    // Make the moving bar
+    rect(200, 790, 20, 20);
 };
