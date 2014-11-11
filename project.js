dd783a2b18a9f4c056a0eebe30f8a6c06304e0f1
EWB User <ewb@unassigned-hostname>
2014-11-11 17:47:01 -0500

    message

--- project.js
+++ project.js
@@ -2,25 +2,68 @@
 title('Project-Breakout');
 
 // Variables
+// Bar Coordinates
 var xCoord = 0;
 var yCoord = 0;
+// Ball Coordinates
+var xBallCoord = 0;
+var yBallCoord = 0;
+// Speed
+var speed = 5;
+// Boundries
+var rBound = 400;
+var lBound = 400;
 
-// this code is executed once when the program is started
-var setup = function() {
-
-    // set up the size of the canvas (you probably don't want to change this!)
+// This code is executed once when the program is started
+var setup = function()
+{
+    // set up the size of the canvas
     size(400,400);
 };
 
-// override draw function, by default it will be called 60 times per second
-var draw = function() {
-
-    // clear the screen
+// Override draw function, by default it will be called 60 times per second
+var draw = function()
+{
+    // Clear the screen
     background(255, 255, 255);
 
-    // some drawing commands; feel free to change these
-    fill(255, 0, 0);
+    // Fill the objects
+    fill(68, 166, 1);
 
-    // Make the moving bar
-    rect(200, 790, 20, 20);
-};
+    // Draw the moving bar
+    rect(xCoord + 200, yCoord + 370, 45, 8);
+    
+    // Make the bar move
+    // Left
+    if(keyIsPressed && keyCode === RIGHT)
+    {
+    xCoord += 5;
+    };
+    // Right
+    if(keyIsPressed && keyCode === LEFT)
+    {
+    xCoord -= 5;
+    };
+
+    // Make the bar tele if it moves off of screen
+  //if()
+  //{
+    
+  //}
+  //if()
+  //{
+    
+  //}
+
+    // The ball
+    ellipse(xBallCoord, yBallCoord, 10, 10);
+    
+    // Make the ball move
+    yBallCoord = yBallCoord + 2;
+    if(yBallCoord >)
+    {
+    
+    };
+
+//Call the functions;
+
